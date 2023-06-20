727d5f19dd39a42a91452f61db95c3724e4f2473
Janussr <Janusstivang@gmail.com>
2023-06-20 16:31:14 +0200

    initial commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,74 @@
+﻿using CodeFirstProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeFirstProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        private readonly UserContext _userContext;
+
+        public UsersController(UserContext userContext)
+        {
+            _userContext = userContext;
+        }
+
+
+        [HttpGet]
+        [Route("GetUsers")]
+        public List<Users> GetUsers()
+        {
+            return _userContext.Users.ToList();
+        }
+
+
+        [HttpGet]
+        [Route("getuser")]
+        public Users GetUser(int id)
+        {
+            return _userContext.Users.Where(x => x.ID == id).FirstOrDefault();
+        }
+
+        [HttpPost]
+        [Route("AddUser")]
+        public string AddUser(Users users)
+        {
+            string response = string.Empty;
+            _userContext.Users.Add(users);
+            _userContext.SaveChanges();
+            return "Users added";
+        }
+
+
+        [HttpPut]
+        [Route("UpdateUser")]
+        public string UpdateUser(Users users)
+        {
+            _userContext.Entry(users).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _userContext.SaveChanges();
+            return "User updated";
+        }
+
+
+        [HttpDelete]
+        [Route("deleteUser")]
+        public string DeleteUser(int id)
+        {
+            Users users = _userContext.Users.Where(u => u.ID == id).FirstOrDefault();
+            if (users != null)
+            {
+                _userContext.Users.Remove(users);
+                _userContext.SaveChanges();
+                return "User deleted";
+            }
+            else
+            {
+                return "No user found";
+            }
+        }
+
+    }
+}
---
+++ Models/UserContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstProject.Models
+{
+    public class UserContext :DbContext
+    {
+        public UserContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Users> Users { get; set; }
+
+    }
+}
